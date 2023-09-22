52430d40912766b66fc3e456db1cf0465af52b50
Naiim Taefy <naiim.taefy.company@gmail.com>
2023-09-22 09:13:00 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,39 +27,70 @@ function renderEverything() {
   renderGreenPeppers();
   renderWhiteSauce();
   renderGlutenFreeCrust();
-
   renderButtons();
   renderPrice();
 }
 
-function renderPepperoni() {
-  document.querySelectorAll('.pep').forEach((onePep) => {
-    if (state.pepperoni) {
-      onePep.style.visibility = 'visible';
+function renderSelection(ingClass, ingState) {
+  const ing = document.querySelectorAll(ingClass);
+  for (const oneIng of ing) {
+    if (state[ingState]) {
+      oneIng.style.visibility = 'visible';
     } else {
-      onePep.style.visibility = 'hidden';
+      oneIng.style.visibility = 'hidden';
     }
-  });
+  }
+}
+
+function renderPepperoni() {
+  renderSelection('.pep', 'pepperoni');
 }
 
 function renderMushrooms() {
   // Iteration 1: set the visibility of `<section class="mushroom">`
+  renderSelection('.mushroom', 'mushrooms');
 }
 
 function renderGreenPeppers() {
   // Iteration 1: set the visibility of `<section class="green-pepper">`
+  renderSelection('.green-pepper', 'greenPeppers');
+}
+
+function renderAddRem(ingClass, ingState, ingClassEdit) {
+  const ingred = document.querySelector(ingClass);
+  if (state[ingState]) {
+    ingred.classList.add(ingClassEdit);
+  } else {
+    ingred.classList.remove(ingClassEdit);
+  }
 }
 
 function renderWhiteSauce() {
   // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  renderAddRem('.sauce', 'whiteSauce', 'sauce-white');
 }
 
 function renderGlutenFreeCrust() {
   // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  renderAddRem('.crust', 'glutenFreeCrust', 'crust-gluten-free');
+}
+
+function renderButton(btnClass, stateName) {
+  const btn = document.querySelector(btnClass);
+  if (state[stateName]) {
+    btn.classList.add('active');
+  } else {
+    btn.classList.remove('active');
+  }
 }
 
 function renderButtons() {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+  renderButton('.btn-pepperoni', 'pepperoni');
+  renderButton('.btn-mushrooms', 'mushrooms');
+  renderButton('.btn-green-peppers', 'greenPeppers');
+  renderButton('.btn-sauce', 'whiteSauce');
+  renderButton('.btn-crust', 'glutenFreeCrust');
 }
 
 function renderPrice() {
@@ -69,15 +100,33 @@ function renderPrice() {
 renderEverything();
 
 // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperoni">`
-document.querySelector('.btn.btn-pepperoni').addEventListener('click', function () {
-  state.pepperoni = !state.pepperoni;
+document
+  .querySelector('.btn.btn-pepperoni')
+  .addEventListener('click', function () {
+    state.pepperoni = !state.pepperoni;
+    renderEverything();
+  });
+// Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
+document
+  .querySelector('.btn.btn-mushrooms')
+  .addEventListener('click', function () {
+    state.mushrooms = !state.mushrooms;
+    renderEverything();
+  });
+// Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
+document
+  .querySelector('.btn.btn-green-peppers')
+  .addEventListener('click', function () {
+    state.greenPeppers = !state.greenPeppers;
+    renderEverything();
+  });
+// Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
+document.querySelector('.btn.btn-sauce').addEventListener('click', function () {
+  state.whiteSauce = !state.whiteSauce;
   renderEverything();
 });
-
-// Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
-
-// Iteration 1: Add click event listener on `<button class="btn btn-green-peppers">`
-
-// Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
-
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+document.querySelector('.btn.btn-crust').addEventListener('click', function () {
+  state.glutenFreeCrust = !state.glutenFreeCrust;
+  renderEverything();
+});
